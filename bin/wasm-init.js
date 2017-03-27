cd97912b4135349c2106d68eb21ab210a2ef1212
Matthias Wagner <matzewagner@gmail.com>
2017-03-27 14:22:36 -0700

    refactored compileWASM into separate file in lib

--- bin/wasm-init.js
+++ bin/wasm-init.js
@@ -5,8 +5,10 @@ const exec = require('child_process').exec;
 const colors = require('colors');
 const create = require('./../lib/createFiles');
 const templates = require('./../lib/templateFileContent');
+const cc = require('./../lib/compileWASM');
 
 process.stdout.write(colors.cyan('Creating WASM template...\n'));
+
 
 const argsArr = process.argv.slice(2);
 const args = {};
@@ -42,64 +44,4 @@ if (args['clean']) {
 const config = require('./../../../wasm.config.js');
 // const config = require('./../wasm.config.js');
 
-function compileWASM () {
-  // check that emscripten path is correct
-  if (!fs.existsSync(config.emscripten_path)) return process.stdout.write(colors.red(`Error: Could not find emscripten directory at ${config.emscripten_path}\n`));
-  // format exported functions from config for shell script
-  let expFuncs = config.exported_functions.reduce((acc, val) => acc.concat('\'', val, '\'\,'), '[');
-  expFuncs = expFuncs.substring(0, expFuncs.length - 1).concat(']');
-
-  // format flags from config for shell script
-  const flags = config.flags.reduce((acc, val) => acc.concat(' ', val), '');
-
-  // execute shell script
-  exec(`
-  if [[ :$PATH: != *:"/emsdk":* ]]
-  then
-    # use path to emsdk folder, relative to project directory
-    BASEDIR="${config.emscripten_path}"
-    EMSDK_ENV=$(find "$BASEDIR" -type f -name "emsdk_env.sh")
-    source "$EMSDK_ENV"
-  fi
-
-  # add exported C/C++ functions here
-  CPP_FUNCS="[
-  '_myFunc',
-  ]"
-
-  echo "Compiling C++ to WASM ..."
-  echo " "
-  emcc -o ${config.outputfiles} ${config.inputfiles} \
-  -s EXPORTED_FUNCTIONS="${expFuncs}" \
-  ${flags}
-
-  `, (err, stdout) => {
-    if (err) process.stderr.write(colors.white(err));
-    process.stdout.write(stdout);
-    insertEventListener();
-  });
-}
-
-// insert event dispatcher into lib.js,
-// to notify us when the script is done loading
-function insertEventListener () {
-  fs.readFile('./wasm/lib.js', 'utf-8', (err1, data) => {
-    if (err1) process.stderr.write(colors.white(err1));
-    
-    data = data.replace(/else\{doRun\(\)\}/g, 'else{doRun()}script.dispatchEvent(doneEvent);');
-    
-    fs.writeFile('./wasm/lib.temp.js', data, (err2) => {
-      if (err2) process.stderr.write(colors.white(err2));
-      fs.renameSync('./wasm/lib.temp.js', './wasm/lib.js');
-    });
-  });
-}
-
-compileWASM();
-
-clean = function() {
-  exec(`rm ./wasm/*.* `, (err, stdout) => {
-    if (err) process.stderr.write(err);
-    process.stdout.write(stdout);
-  });
-};
+cc.compileWASM(config);

7a9bc72e7910da3a57b8a936b24a705d72eb42f8
Matthias Wagner <matzewagner@gmail.com>
2017-04-07 15:56:21 -0700

    refactoring and added comments

--- bin/wasm-compile.js
+++ bin/wasm-compile.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
+/**
+ * This file calls the compileWASM function, which calls
+ * the emmcc shell commands with the environment variables
+ * from the wasm.config.js file.
+ * */
+
 const path = require('path');
-const colors = require('colors');
 const cc = require('./../lib/compileWASM');
 const config = require(path.join(process.cwd(), './wasm.config.js'));
 
